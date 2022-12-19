d0261becb76f55e10eca9d6587387182c82475c7
ctbus <ctbushman@gmail.com>
2022-12-19 13:07:24 -0500

    Update snakemake version because conda is the worst

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ bash ${tmpdir}/miniconda.sh -b -p ${__conda_path}
 rm ${tmpdir}/miniconda.sh
 
 conda install --yes --quiet -n base -c conda-forge mamba
-mamba install --yes --quiet -n base -c bioconda snakemake=3.17
+conda install --yes --quiet -n base -c bioconda snakemake=7
