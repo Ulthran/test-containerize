97edfea295728a8d163b2766a16a6e25e111f861
ctbus <ctbushman@gmail.com>
2022-12-19 12:17:29 -0500

    Add test

---
+++ install.sh
@@ -0,0 +1,14 @@
+__conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+__conda_path="${HOME}/miniconda3"
+__old_path=$PATH
+PATH=$PATH:${__conda_path}/bin
+
+local tmpdir=$(mktemp -d)
+echo "Downloading miniconda..."
+wget -nv ${__conda_url} -O ${tmpdir}/miniconda.sh
+echo "Installing miniconda..."
+bash ${tmpdir}/miniconda.sh -b -p ${__conda_path}
+rm ${tmpdir}/miniconda.sh
+
+conda install --yes --quiet -n base -c conda-forge mamba
+mamba install --yes --quiet -n base -c bioconda snakemake
